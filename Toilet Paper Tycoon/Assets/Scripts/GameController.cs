fdaf1c13886165bffbcb13eb3b6e66974cff115d
Trent1048 <trentbultsma@gmail.com>
2020-05-03 00:09:31 -0700

    make ground tiles interactable and place trees on click

---
+++ Toilet Paper Tycoon/Assets/Scripts/GameController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public static GameController gameController;
+
+    public GameObject selectedObject; // TODO: Add system to change the selected object
+    public GameObject ground;
+
+    private void Awake() {
+
+        if (gameController == null) {
+            gameController = this;
+        } else {
+            Debug.LogError("Cannot have more than one game controller");
+        }
+    }
+}
