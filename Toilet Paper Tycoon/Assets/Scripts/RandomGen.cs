729050bb86acc56e88ed965b205b895b5556cf65
Joe Lindbeck <lindbeck.joe@gmail.com>
2020-05-06 17:15:05 -0700

    tree generator

---
+++ Toilet Paper Tycoon/Assets/Scripts/RandomGen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomGen : MonoBehaviour
+{
+    public GameObject[] objects;
+
+    void Start()
+    {
+        int rand = Random.Range(0, objects.Length);
+        Instantiate(objects[rand], transform.position, Quaternion.identity);
+    }
+
+
+}
