b48fd43537bc0686553c99d9627b7093d1635197
Trent1048 <trentbultsma@gmail.com>
2020-05-12 13:14:32 -0700

    Add basic tree growth template

--- Toilet Paper Tycoon/Assets/Scripts/GameController.cs
+++ Toilet Paper Tycoon/Assets/Scripts/GameController.cs
@@ -36,7 +36,7 @@ public class GameController : MonoBehaviour {
 
     // put anything that runs every tick in this function
     public void GameTick() {
-
+        TreeController.GrowTrees();
     }
 
     public void ChangeSelectedCharacter(GameObject character) {
