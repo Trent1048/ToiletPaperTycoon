d61959066d5129cea9f7159e0f902c35cbdaf459
Trent1048 <trentbultsma@gmail.com>
2020-05-13 16:24:16 -0700

    add the ability to press escape to open the menu

--- Toilet Paper Tycoon/Assets/Scripts/ItemSelectionController.cs
+++ Toilet Paper Tycoon/Assets/Scripts/ItemSelectionController.cs
@@ -6,6 +6,12 @@ public class ItemSelectionController : MonoBehaviour {
     public Image itemIcon;
     public GameObject itemSelectionMenu;
 
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            ToggleMenu();
+        }
+    }
+
     public void ChangeIcon(Sprite sprite, Vector3 scale) {
         itemIcon.sprite = sprite;
         itemIcon.transform.localScale = scale;
