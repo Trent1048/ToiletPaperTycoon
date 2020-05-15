66fe97642d15a13a111176ebe5834e3d9e502588
PieterTims <64707941+PieterTims@users.noreply.github.com>
2020-05-14 18:47:00 -0700

    add conveyor reference another conveyor next to it

--- Toilet Paper Tycoon/Assets/Scripts/ConveyorController.cs
+++ Toilet Paper Tycoon/Assets/Scripts/ConveyorController.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 public class ConveyorController : MonoBehaviour
 {
@@ -17,6 +18,17 @@ public class ConveyorController : MonoBehaviour
     public Sprite[] sprites;
     private SpriteRenderer spriteRenderer;
     private int switchCounter = 0;
+
+    private ContactFilter2D contactFilter;
+    private Direction dir;
+
+    private Dictionary<int, Vector3> offsetDictionary = new Dictionary<int, Vector3>
+    {
+        {0, new Vector3(-0.5f,-0.25f,0)},
+        {1, new Vector3(-0.5f,0.25f,0)},
+        {2, new Vector3(0.5f,0.25f,0)},
+        {3, new Vector3(0.5f,-0.25f,0)}
+    };
 
     // Start is called before the first frame update
     void Start()
@@ -39,16 +51,13 @@ public class ConveyorController : MonoBehaviour
             sprites[0] = spriteRenderer.sprite;
         }
 
+        contactFilter = new ContactFilter2D();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //overlapcircle
-        //checkposition
-        //check using ground tile
-        //usedirection
-        
+      
 
     }
     private void OnDestroy()
@@ -69,6 +78,24 @@ public class ConveyorController : MonoBehaviour
         {
             switchCounter++;
             spriteRenderer.sprite = sprites[switchCounter];
+
+            List<Collider2D> hitColliders = new List<Collider2D>();
+            Physics2D.OverlapCircle(transform.position, 0.5f, contactFilter, hitColliders);
+
+            foreach (Collider2D col in hitColliders)
+            {
+                ConveyorController conveyor = col.GetComponent<ConveyorController>();
+                Vector3 otherPos = new Vector3(col.transform.parent.position.x, col.transform.parent.position.y);
+                Vector3 thisPos = new Vector3(transform.parent.position.x, transform.parent.position.y);
+                if (conveyor != null)
+                {
+                    if (thisPos + offsetDictionary[switchCounter] == otherPos)
+                    {
+                        next = conveyor.next;
+                    }
+                }
+            }
+
             if (switchCounter >= 3)
             {
                 switchCounter = -1;
