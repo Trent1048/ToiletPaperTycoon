19a63b445dfc2d3c0ed8f9b89e1afdd8802174e8
PieterTims <64707941+PieterTims@users.noreply.github.com>
2020-05-29 04:17:49 -0700

    Fixed bug in TPCreator

--- Toilet Paper Tycoon/Assets/Scripts/TpCreator.cs
+++ Toilet Paper Tycoon/Assets/Scripts/TpCreator.cs
@@ -8,16 +8,17 @@ public class TpCreator : ConveyorController
     public GameObject tp;
     public int howMuchTp;
 
+    private int paperNum = 0;
+
     // Update is called once per frame
     void Update()
     {
-        CountPaper(); 
+        if (storedObject) CountPaper(); 
     }
 
 
     public void CountPaper()
     {
-        int paperNum = 0;
         if (storedObject.CompareTag("Tree"))//change to "paper"
         {
             paperNum++;
