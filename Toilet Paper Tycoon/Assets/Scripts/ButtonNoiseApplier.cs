06580cb70c838f25748d4884c65d900b54cdcc37
Trent1048 <trentbultsma@gmail.com>
2020-06-03 21:59:39 -0700

    add music and click sounds

---
+++ Toilet Paper Tycoon/Assets/Scripts/ButtonNoiseApplier.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonNoiseApplier : MonoBehaviour {
+
+    public AudioSource clickSound;
+
+	// loop through all button children and give them sounds
+	private void Start() {
+		Stack<Transform> children = new Stack<Transform>();
+
+		for (int childNum = 0; childNum < transform.childCount; childNum++) {
+			children.Push(transform.GetChild(childNum));
+		}
+
+		int count = 0;
+
+		while (children.Count > 0 && count < 100) {
+			count++;
+
+			Transform currentChild = children.Pop();
+
+			Button currentButton = currentChild.GetComponent<Button>();
+			if (currentButton != null) {
+				currentButton.onClick.AddListener(() => clickSound.Play());
+			}
+
+			for (int childNum = 0; childNum < currentChild.transform.childCount; childNum++) {
+				children.Push(currentChild.transform.GetChild(childNum));
+			}
+		}
+	}
+}
