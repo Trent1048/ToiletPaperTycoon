57b65d53d9c3b35cd58004e541a23ae8cfb37566
Trent1048 <trentbultsma@gmail.com>
2020-06-10 13:24:59 -0700

    add basic tutorial

---
+++ Toilet Paper Tycoon/Assets/Scripts/TipsController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TipsController : MonoBehaviour {
+
+	public string[] instructions;
+	public Text instructionText;
+	private int instructionNum;
+
+	private void Start() {
+		Time.timeScale = 0;
+		instructionNum = 0;
+		instructionText.text = instructions[instructionNum];
+	}
+
+	private void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			instructionNum++;
+			if (instructionNum < instructions.Length) {
+				instructionText.text = instructions[instructionNum];
+			} else {
+				Time.timeScale = 1;
+				gameObject.SetActive(false);
+			}
+		}
+	}
+}
